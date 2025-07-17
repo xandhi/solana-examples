9ed5741f11e34f8f5bad901e5682550c514c6d9f
Xander <xander@example.com>
2025-07-17 13:49:34 +0100

    Initial counter program, untested

--- counter/programs/counter/src/lib.rs
+++ counter/programs/counter/src/lib.rs
@@ -7,10 +7,35 @@ pub mod counter {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+        ctx.accounts.counter_account.data = 0;
+        msg!("Counter set to: 0");
+        Ok(())
+    }
+
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {
+        ctx.accounts.counter_account.data += 1;
+        msg!("Counter incremented to: {}.", ctx.accounts.counter_account.data);
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = signer, space = 8 + 8)]
+    pub counter_account: Account<'info, CounterAccount>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account(mut)]
+    pub counter_account: Account<'info, CounterAccount>,
+    pub signer: Signer<'info>,
+}
+
+#[account]
+pub struct CounterAccount {
+    pub data: u64,
+}
